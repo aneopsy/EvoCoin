046565af4db147ad8b5988cee98049ea00b1c4bb
theis_p <paul.theis@epitech.eu>
2017-10-24 22:41:32 +0200

    modifié :         client/script/geoip.js

--- client/script/geoip.js
+++ client/script/geoip.js
@@ -41,6 +41,7 @@ class GeoIP {
         }
 
         var xmlhttp = new XMLHttpRequest();
+        xmlhttp.withCredentials = false;
         var url = 'https://geoip.nimiq-network.com:8443/v1/locate' + (host && host.length > 0 ? '?host=' + host : '');
 
         xmlhttp.onreadystatechange = function() {
